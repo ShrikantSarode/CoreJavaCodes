b84bc114cfe53fd796452cb99048f08e9cc8f600
Shrikant <shrushti6602@gmail.com>
2024-10-05 09:49:36 +0530

    assignment7

---
+++ JavaProject/src/assignment7/Book.java
@@ -0,0 +1,15 @@
+package assignment7;
+
+public class Book  {
+
+	//tile,author
+	//getter setter, toString\
+	 String title,author;
+	 
+	 public Book(String title,String author) {
+		 this.title=title;
+		 this.author=author;
+	 }
+	
+	
+}
